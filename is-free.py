5ad0f191895436e1578824abcafaef945d1efed5
Bradley Stannard <bysd@gft.com>
2021-10-19 11:00:29 +0100

    added cool outputs

--- is-free.py
+++ is-free.py
@@ -1,8 +1,6 @@
-import requests
-import json
+import requests, json, datetime
 #Opens and reads the token from the file called "token"
 fi = open("token", "r")
-
 
 # Makes a get request to the endpoint below
 url = "https://mobile.laundryrestart.com/jhclaundry/api/catalog/machines/1"
@@ -18,8 +16,21 @@ data = response.json()
 query = json.dumps(data)
 a = json.loads(query)
 
-# For loop to show the status of the machines
+# For loop to show the status of the machines and time left where it's relevant
 for s in range(6):
-   print("machine {} is {}.".format(a[s]["name"], a[s]["isAvailable"]))
+
+   if a[s]["statusName"] == "Reserved": #should be Reserved
+
+    tl = str(datetime.timedelta(seconds=a[s]["secLeft"]))
+    print("machine {}".format(a[s]["name"]),"is Reserved and has", tl, "remaining")
+
+   elif a[s]["statusName"] == "Available":
+
+    print("machine {} is {}.".format(a[s]["name"], a[s]["statusName"]))
+
+   elif a[s]["statusName"] == "Busy": #should be Busy
+
+    tl = str(datetime.timedelta(seconds=a[s]["secLeft"]))
+    print("machine {}".format(a[s]["name"]),"is Busy and has", tl, "remaining")
 
 fi.close()
